f5ef531d8442406928e464b8e872b16a7decfee3
Jay Arredondo <jearredondo13@gmail.com>
2021-06-02 15:53:14 -0500

    finished bonuses for util and shapes package

--- src/util/Input.java
+++ src/util/Input.java
@@ -14,12 +14,31 @@ public class Input {
         return this.scanner.nextLine();
     }
 
+    // TODO: **BONUS**
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        return this.scanner.nextLine();
+    }
+
     public boolean yesNo() {
         String userInput = this.scanner.nextLine();
         return userInput.trim().equalsIgnoreCase("y") || userInput.trim().equalsIgnoreCase("yes");
     }
 
+    // TODO: **BONUS**
+    public boolean yesNo(String prompt) {
+        System.out.println(prompt);
+        String userInput = this.scanner.next();
+        return userInput.trim().equalsIgnoreCase("y") || userInput.trim().equalsIgnoreCase("yes");
+    }
+
     public int getInt() {
+        return this.scanner.nextInt();
+    }
+
+    // TODO: **BONUS**
+    public int getInt(String prompt) {
+        System.out.println(prompt);
         return this.scanner.nextInt();
     }
 
@@ -35,7 +54,27 @@ public class Input {
         }
     }
 
+    // TODO: **BONUS**
+    public int getInt(String prompt, int min, int max) {
+        System.out.println(prompt);
+        int userNumber = this.scanner.nextInt();
+
+        if(userNumber >= min && userNumber <= max) {
+            return userNumber;
+        } else {
+            System.out.println("That number is invalid.");
+            return getInt(min, max);
+        }
+    }
+
+
     public double getDouble() {
+        return this.scanner.nextDouble();
+    }
+
+    // TODO: **BONUS**
+    public double getDouble(String prompt) {
+        System.out.println(prompt);
         return this.scanner.nextDouble();
     }
 
@@ -50,6 +89,20 @@ public class Input {
             return getDouble(min, max);
         }
     }
+
+    // TODO: **BONUS**
+    public double getDouble(String prompt, double min, double max) {
+        System.out.println(prompt);
+        double userNumber = this.scanner.nextDouble();
+
+        if(userNumber >= min && userNumber <= max) {
+            return userNumber;
+        } else {
+            System.out.println("That number is invalid.");
+            return getDouble(min, max);
+        }
+    }
+
 
 
 
